7bb05ef0f9308af2cf47396f8583327802f9057d
opilane <opilane@HP-CZC3217062>
2025-09-04 12:44:55 +0300

    Add project files.

---
+++ TallinnaRakenduslikKolledž/Models/Enrollment.cs
@@ -0,0 +1,17 @@
+﻿namespace TallinnaRakenduslikKolledž.Models
+{
+    public enum Grade
+    {
+        A,B,C,D,F,X,MA
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID {  get; set; }
+        public int CourseID {  get; set; }
+        public int StudentID {  get; set; }
+        public Grade? CurrentGrade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ TallinnaRakenduslikKolledž/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallinnaRakenduslikKolledž.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+        /*lisa kolm omadust õpilasele, ise mõtled välja*/
+        public int Course {  get; set; }
+        public string Gender {  get; set; }
+        public int? Phone {  get; set; }
+    }
+}
