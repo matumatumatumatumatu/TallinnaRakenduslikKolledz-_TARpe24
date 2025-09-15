f8d0a5b6d02eecdd24815fbacc5aef1aec4ecdc8
opilane <opilane@HP-CZC3217062>
2025-09-15 14:57:27 +0300

    a

--- TallinnaRakenduslikKolledž/Controllers/InstructorsController.cs
+++ TallinnaRakenduslikKolledž/Controllers/InstructorsController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TallinnaRakenduslikKolledž.Data;
+using TallinnaRakenduslikKolledž.Models;
 
 namespace TallinnaRakenduslikKolledž.Controllers
 {
     public class InstructorsController : Controller
     {
-        public IActionResult Index()
+        private readonly SchoolContext _context;
+        public InstructorsController( SchoolContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int? id, int? courseId)
+        {
+            var vm = new InstructorIndexData();
+            vm.Instructors = await _context.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments)
+                .ToListAsync();
+            return View(vm);
         }
     }
 }
