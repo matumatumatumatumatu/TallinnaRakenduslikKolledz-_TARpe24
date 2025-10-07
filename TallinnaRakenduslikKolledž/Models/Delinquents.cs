17ae8ef8977e08fadde6cb0496e970805c9a575f
opilane <opilane@HP-CZC3217062>
2025-10-07 14:09:52 +0300

    delinquents model

---
+++ TallinnaRakenduslikKolledž/Models/Delinquents.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallinnaRakenduslikKolledž.Models
+{
+
+    public class Delinquents
+    {
+        [Key]
+        public int DelinquentsID { get; set; }
+       public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public enum Violation
+        {
+            UrinalShitting,Smoking,Vandalizing,Gooning,Murder
+        }
+        public bool Instructor { get; set; }
+        public string Details { get; set; }
+    }
+}
+//
